722368518c200d698f8c57e3751adb242de58b5d
Varad <varadrane1707@gmail.com>
2023-04-07 22:15:48 +0530

    Data processing

---
+++ nltk_utils.py
@@ -0,0 +1,33 @@
+import nltk
+
+# nltk.download('punkt')
+
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence.lower())
+
+def stem(word):
+    return nltk.PorterStemmer().stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence , all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+
+# a = " How is my health?"
+# print(tokenize(a))
+
+b = ["How", "is", "my", "health"]
+
+words = ["orgnize","organization","organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
+
---
+++ train.py
@@ -0,0 +1,24 @@
+import json
+from nltk_utils import tokenize,stem,bag_of_words
+
+with open('intents.json') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intents[0]['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+
+print(xy)
+# ignore_words = ['?','!','.',',']
+
+
+
