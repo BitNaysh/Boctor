107c5fa740836ede3844796da96e39b90e798416
Varad <varadrane1707@gmail.com>
2023-04-08 22:57:13 +0530

    web app developed

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+# import torch.multiprocessing
+# torch.multiprocessing.set_start_method('spawn')
+
 from model import NeuralNet
 
 with open('intents.json') as f:
@@ -67,7 +70,7 @@ learning_rate = 0.001
 num_epochs = 1000
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size,shuffle=True,num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size,shuffle=True,num_workers=0)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -109,6 +112,24 @@ for epoch in range(num_epochs):
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 print(f'final loss : , loss={loss.item():.4f}')
+
+
+data = { 
+"model_state": model.state_dict(),
+"input_size": input_size,
+"output_size": output_size,
+"hidden_size": hidden_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'Training complete. file saved to {FILE}')
+
+
+
 
 
 
